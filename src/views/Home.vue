4820fee3e8a443644712ca27bcd1a2aa5db7c190
Emmanuel Daniels <emmanueldaniels37@gmail.com>
2020-12-16 23:11:23 -0500

    added images login and signup page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+<style>
+body {
+  background-image: url("../assets/images/spiderman_wallpaper.jpg");
 }
+</style>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "Welcome to the Spider Verse!",
+    };
+  },
+  created: function () {},
+  methods: {},
+};
 </script>
+
